b24f6f9e989641f249e0700eb08699c3f0a33024
artjiyr <106913775+artjiyr@users.noreply.github.com>
2025-12-05 09:57:31 -0300

    vish, esqueci de salvarkkk

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ dots.forEach((dot, index) => {
 window.addEventListener("resize", reloadSlider);
 
 // autoplay
-setInterval(() => next.click(), 8000);
+setInterval(() => next.click(), 7000);
 
 // iniciar
 reloadSlider();
